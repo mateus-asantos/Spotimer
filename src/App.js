12c7e8711fc55e2123c7879ed9a8869acf4c7295
sleeip <m4uk13@gmail.com>
2019-02-20 11:55:07 -0300

    Revert "authorization (#7)"
    This reverts commit 2c57b0e9b44af4f32df0dd407ae20da741e28e2e.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Login } from './components/login';
-import Playlists from './components/Playlists';
 
 class App extends Component {
-  
-
-
   render() {
-    console.log(window.location.hash)
     return (
       <div className="App">
-        {window.location.hash?<Playlists access_token={window.location.hash} />:<Login />}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
